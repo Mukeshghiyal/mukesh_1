39dc6d74ef6c5ee690b56ab328dff8fd5bcdb27b
mukeshghiyal <mkghiyal7@gmail.com>
2025-07-11 16:50:14 +0530

    3rd rg

--- main.tf
+++ main.tf
@@ -3,7 +3,14 @@ resource "azurerm_resource_group" "rg_name1" {
   location = "east us"
 }
 
+resource "azurerm_resource_group" "rg_name2" {
+  name="mk-rg2"
+  location = "east us"
+}
+
+
 resource "azurerm_resource_group" "rg_name" {
   name="mk-rg"
   location = "east us"
 }
+
