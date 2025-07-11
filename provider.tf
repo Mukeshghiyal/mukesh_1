faa52862fba13b507989ba3c6d326dfcc8ebb537
mukeshghiyal <mkghiyal7@gmail.com>
2025-07-11 13:52:29 +0530

    1 rg code

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm={
+
+source = "hashicorp/azurerm"
+version = ">3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+
+    features {}
+    subscription_id ="377674hhd4773ggg4777477ggg3" 
+  
+}
